12b02751def2191f736e76f7346946694db1167c
amcspps <pashafizmat1@gmail.com>
2024-01-20 16:26:41 +0300

    cmakelists fixed, device tests passed

--- src/parallel_executor/DeviceA.h
+++ src/parallel_executor/DeviceA.h
@@ -1,3 +1,7 @@
+
+#include <sstream>
+#include <thread>
+#include <chrono>
 #ifndef DEVICEA_H
 #define DEVICEA_H
 
@@ -7,12 +11,12 @@ class DeviceA : public Device
 {
 public:
   std::string getName() override {
-    return "Device A";
+    return "A";
   };
   std::string getDataAsString() override {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
     std::stringstream ss;
-    ss << "Data from " << getName() << ": " << rand() % 501;
+    ss << "string from a length <= 500";
     return ss.str();
   };
 };
--- src/parallel_executor/DeviceB.h
+++ src/parallel_executor/DeviceB.h
@@ -6,11 +6,13 @@
 class DeviceB : public Device
 {
 public:
-  std::string getName() override;
+  std::string getName() override {
+    return "B";
+  };
   std::string getDataAsString() override {
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
     std::stringstream ss;
-    ss << "Data from " << getName() << ": " << rand() % 199 << " " << rand() % 199 << " " << rand() % 199;
+    ss << rand() % 199 << " " << rand() % 199 << " " << rand() % 199;
     return ss.str();
   };
 };
